b76db2ca35686f0e4e7a08bfbf74d689522f8384
Oscar Nicoulaz <nicoulaz.oscar@hotmail.com>
2021-08-03 17:33:02 +0200

    oniouubiivzuct

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,13 +7,24 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+    # @task = Task.new(params[:task])
+  end
+
   def create
-    @task = Task.new()
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
   end
 
   def update
   end
 
   def delete
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
