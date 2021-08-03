d44d4a47a18379dd5ed93af79fca0b1d55864cd0
Oscar Nicoulaz <nicoulaz.oscar@hotmail.com>
2021-08-03 13:51:29 +0200

    ifvui

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,19 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new()
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
